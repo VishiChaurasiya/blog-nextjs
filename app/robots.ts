4ec8c9b851b9124f3ce2ebc119a76bc1e9fa2106
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2024-03-23 18:31:47 +0530

    Add robots.ts and sitemap.ts for seo

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/privacy"],
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/sitemap.xml`,
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+import { getPosts } from "./actions/getPosts";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getPosts();
+  const slugs = posts.map((post) => post.slug);
+
+  const postEntries: MetadataRoute.Sitemap = slugs.map((slug) => ({
+    url: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/blog/${slug}`,
+    // lastModified: new Date(post.updatedAt),
+    // changeFrequency:,
+    // priority:
+  }));
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/about`,
+    },
+    ...postEntries,
+  ];
+}
